a18352955067c88228a7bc4acfb89a35ff5857ea
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-02-23 09:48:23 -0600

    Minor bug fix

--- src/components/viewer/ResumeHeader.jsx
+++ src/components/viewer/ResumeHeader.jsx
@@ -1,6 +1,6 @@
 function ResumeHeader({ data }) {
   const headerKey = data?.["generalData"]["children"][0];
-  const headerData = data[headerKey];
+  const headerData = data[headerKey] ?? [];
 
   const headerLine1 =
     (headerData?.["firstName"] ? headerData["firstName"] + " " : "") +
